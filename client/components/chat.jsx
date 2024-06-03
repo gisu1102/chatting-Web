ed92c2b0fbbdb205d5596dce8340500f7da0aa6d
gisu1102 <gisu1102@gmail.com>
2024-06-03 21:49:01 +0900

    .

--- client/components/chat.jsx
+++ client/components/chat.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import ChatRoomList from './ChatRoomList.jsx';
 import ChatRoom from './ChatRoom.jsx';
 import Sidebar from './Sidebar.jsx';
-import TopBar from './TopBar.jsx';
 import '../styles/Chat.css';
+import Header from './Header';
 
 const Chat = ({ user }) => {
     const [selectedRoom, setSelectedRoom] = useState(null);
+    const [reset, setReset] = useState(false);
 
     const handleSelectRoom = (roomName) => {
         setSelectedRoom(roomName);
@@ -14,6 +15,8 @@ const Chat = ({ user }) => {
 
     const handleLogoClick = () => {
         setSelectedRoom(null);
+        setReset(true);
+        setTimeout(() => setReset(false), 0); // Trigger a reset in ChatRoomList
     };
 
     const handleUserInfoClick = () => {
@@ -22,16 +25,23 @@ const Chat = ({ user }) => {
 
     return (
         <div className='chat'>
-            <div className='chat-header'>
-                <TopBar />
+            <div className='chat-header-container'>
+                <Header />
             </div>
             <div className='chat-body'>
                 <div className='chat-sidebar'>
-                    <Sidebar onLogoClick={handleLogoClick} onUserInfoClick={handleUserInfoClick} />
+                    <Sidebar
+                        onLogoClick={handleLogoClick}
+                        onUserInfoClick={handleUserInfoClick}
+                        onParticipantsClick={() => {}}
+                    />
                 </div>
                 <div className='chat-content'>
                     <div className='chat-room-list-container'>
-                        <ChatRoomList onSelectRoom={handleSelectRoom} />
+                        <ChatRoomList
+                            onSelectRoom={handleSelectRoom}
+                            reset={reset}
+                        />
                     </div>
                     <div className='chat-room-container'>
                         {selectedRoom ? (
